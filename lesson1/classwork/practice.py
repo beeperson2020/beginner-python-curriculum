7d4a967b00aeb9123bdb63470bcbfbb9b8c1339c
beeperson2020 <brookeyguo@gmail.com>
2026-09-16 02:36:40 +0000

    lesson1 homework

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,22 +1,26 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
+x = 456
+print(x)
 
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-
+x=1.2
+print("my height is" ,x ,  " meters!!!")
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
+x="dog"
+print("my favorite animal is" ,x, " dog")
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
+a ="Brooke"
+b = "Guo"
 
 
 # Problem 5
--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,30 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+lucky=5
+print("my lucky number is ", lucky)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+sleep=9
+print ("i sleep for",sleep,"hours")
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+fruit="peach"
+print("my favorite fruit is",fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
-
+a="seattle"
+b="china"
+print("i live in",a)
+print("my country is",b)
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+a="bubble" 
+b=4
+c="fish"
+print("I have a ",c, ", his name is ",a,"and he is",b , "years old.")
